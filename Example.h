a4177dce2c859701cf5affe9d5670f8f6ec756ac
consultit <consultit@katamail.com>
2011-03-31 11:25:35 +0000

    * OdeOgre Demo_GranTurismOgre sample completed.

--- Example.h
+++ Example.h
@@ -8,15 +8,53 @@
 #ifndef EXAMPLE_H_
 #define EXAMPLE_H_
 
+#include "OgreOde_Core.h"
+#include "OgreOde_Prefab.h"
+#include "OgreOde_Loader.h"
+
 #include "ExampleApplication.h"
 
-class Example: public ExampleApplication
+class GranTurismOgreFrameListener: public ExampleFrameListener
 {
 public:
-	Example();
-	virtual ~Example();
+	GranTurismOgreFrameListener(RenderWindow* win, Camera* cam, Real time_step,
+			Root* root, OgreOde::World *world);
+	~GranTurismOgreFrameListener();
+	bool frameStarted(const FrameEvent& evt);
+	void changeCar();
 
-	void createScene();
+private:
+	void updateInfo();
+	OgreOde::StepHandler *_stepper;
+	OgreOde::World *_world;
+	OgreOde_Prefab::Vehicle *_vehicle;
+	char _drive;
+	OgreOde_Loader::DotLoader *dotOgreOdeLoader;
+};
+
+class GranTurismOgreApplication: public ExampleApplication,
+		public OgreOde::CollisionListener
+{
+public:
+	GranTurismOgreApplication();
+	~GranTurismOgreApplication();
+
+protected:
+
+	virtual void chooseSceneManager(void);
+	virtual void setupResources(void);
+	virtual void createCamera(void);
+	void createScene(void);
+	void createFrameListener(void);
+
+	virtual bool collision(OgreOde::Contact* contact);
+
+protected:
+	OgreOde::World *_world;
+	OgreOde_Prefab::Vehicle *_vehicle;
+	OgreOde::TriangleMeshGeometry *_track;
+
+	Real _time_step;
 };
 
 #endif /* EXAMPLE_H_ */
