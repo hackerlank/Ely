daf2542da74535136cb69eb9578e5801a05d1b6f
consultit <consultit@katamail.com>
2011-04-07 10:20:45 +0000

    * "OGRE 3D 1.7 Beginners Guide" Examples from chapter 2 completed.

--- Example.cpp
+++ Example.cpp
@@ -20,87 +20,128 @@ Example::~Example()
 
 void Example::createScene()
 {
-	/** Create ambient light */
-	mSceneMgr->setAmbientLight(ColourValue(1, 1, 1));
+	//Reference Model
+	Ogre::Entity* ent = mSceneMgr->createEntity("MyEntity", "Sinbad.mesh");
+	Ogre::SceneNode* node = mSceneMgr->createSceneNode("Node1");
+	mSceneMgr->getRootSceneNode()->addChild(node);
+	node->attachObject(ent);
 
-	/** Create point light, set its type and position */
-	Light *light = mSceneMgr->createLight("Light1");
-	light->setType(Light::LT_POINT);
-	light->setPosition(Vector3(0, 150, 250));
+	//<Part 1 - Setting the position of a scene node
+	//	node->setPosition(10, 0, 0);
+	//
+	//	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+	//	Ogre::SceneNode* node2 = mSceneMgr->createSceneNode("Node2");
+	//	node->addChild(node2);
+	//	node2->setPosition(0, 10, 20);
+	//	node2->attachObject(ent2);
+	// Part 1>
 
-	/** set the diffuse and specular colour */
-	light->setDiffuseColour(1.0, 1.0, 1.0);
-	light->setSpecularColour(1.0, 1.0, 1.0);
+	//<Part 2 - Rotating a scene node
+	//	node->setPosition(10, 10, 0);
+	//	mSceneMgr->getRootSceneNode()->addChild(node);
+	//
+	//	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+	//	Ogre::SceneNode* node2 = mSceneMgr->createSceneNode("Node2");
+	//	node->addChild(node2);
+	//	node2->setPosition(10, 0, 0);
+	//	node2->pitch(Ogre::Radian(Ogre::Math::HALF_PI));
+	//	node2->attachObject(ent2);
+	//
+	//	Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+	//	Ogre::SceneNode* node3 = mSceneMgr->createSceneNode("Node3");
+	//	node->addChild(node3);
+	//	node3->translate(20, 0, 0);
+	//	node3->yaw(Ogre::Degree(90.0f));
+	//	node3->attachObject(ent3);
+	//
+	//	Ogre::Entity* ent4 = mSceneMgr->createEntity("MyEntity4", "Sinbad.mesh");
+	//	Ogre::SceneNode* node4 = mSceneMgr->createSceneNode("Node4");
+	//	node->addChild(node4);
+	//	node4->setPosition(30, 0, 0);
+	//	node4->roll(Ogre::Radian(Ogre::Math::HALF_PI));
+	//	node4->attachObject(ent4);
+	// Part 2>
 
-	mCamera->setPosition(Vector3(0, 120, 800));
+	//<Part 3 - Scaling a scene node
+	//	node->setPosition(10, 10, 0);
+	//
+	//	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+	//	Ogre::SceneNode* node2 = node->createChildSceneNode("node2");
+	//	node2->setPosition(10, 0, 0);
+	//	node2->attachObject(ent2);
+	//	node2->scale(2.0f, 2.0f, 2.0f);
+	//
+	//	Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+	//	Ogre::SceneNode* node3 = node->createChildSceneNode("node3", Ogre::Vector3(
+	//			20, 0, 0));
+	//	node3->attachObject(ent3);
+	//	node3->scale(0.2f, 0.2f, 0.2f);
+	// Part 3>
 
-	mWorld = new OgreOde::World(mSceneMgr);
-	mWorld->setGravity(Ogre::Vector3(0, -9.80665, 0));
-	mWorld->setCFM(10e-5);
-	mWorld->setERP(0.8);
-	mWorld->setAutoSleep(true);
-	mWorld->setAutoSleepAverageSamplesCount(10);
-	mWorld->setContactCorrectionVelocity(1.0);
-	mSpace = mWorld->getDefaultSpace();
+	//<Part 4 - Using a scene graph the clever way
+	//	//	node->setPosition(10,10,0);
+	//
+	//	Ogre::Entity* ent2 =
+	//			mSceneMgr->createEntity("MyEntitysNinja", "ninja.mesh");
+	//	Ogre::SceneNode* node2 = node->createChildSceneNode("node2");
+	//	node2->setPosition(10, 0, 0);
+	//	node2->setScale(0.02f, 0.02f, 0.02f);
+	//	node2->attachObject(ent2);
+	//
+	//	node->setPosition(40, 10, 0);
+	//	node->yaw(Ogre::Degree(180.0f));
+	// Part 4>
 
-	const Real _time_step = 0.5;
-	const Real time_scale = Real(1.7);
-	const Real max_frame_time = Real(1.0 / 4);
-	mStepper = new OgreOde::StepHandler(mWorld,
-			OgreOde::StepHandler::QuickStep, _time_step, max_frame_time,
-			time_scale);
+	//<Part 5 - Different spaces in a scene
+	//	node->setPosition(0, 0, 400);
+	//	node->yaw(Ogre::Degree(180.0f));
+	//
+	//	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+	//	Ogre::SceneNode* node2 = node->createChildSceneNode("node2");
+	//	node2->setPosition(10, 0, 0);
+	//	node2->translate(0, 0, 10);
+	//	node2->attachObject(ent2);
+	//	Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+	//	Ogre::SceneNode* node3 = node->createChildSceneNode("node3");
+	//	node3->setPosition(20, 0, 0);
+	//	//	node3->translate(0, 0, 10);
+	//	node3->translate(0, 0, 10, Ogre::Node::TS_WORLD);
+	//	node3->attachObject(ent3);
+	// Part 5>
 
-	mGround = new OgreOde::InfinitePlaneGeometry(Plane(Ogre::Vector3(0, 1, 0),
-			0), mWorld, mWorld->getDefaultSpace());
-	// Use a load of meshes to represent the floor
-	int i = 0;
-	StaticGeometry* s = mSceneMgr->createStaticGeometry("StaticFloor");
-	s->setRegionDimensions(Vector3(1600.0, 100.0, 1600.0));
-	// Set the region origin so the center is at 0 world
-	s->setOrigin(Vector3::ZERO);
-	for (Real z = -800.0; z <= 800.0; z += 20.0)
-	{
-		for (Real x = -800.0; x <= 800.0; x += 20.0)
-		{
-			String name = String("Ground") + StringConverter::toString(i++);
-			Entity *entity = mSceneMgr->createEntity(name, "plane.mesh");
-			entity->setQueryFlags(1 << 4);
-			entity->setUserAny(Any(mGround));
-			entity->setCastShadows(false);
-			s->addEntity(entity, Vector3(x, 0, z));
-		}
-	}
-	s->build();
+	//<Part 6 - Translating in local space
+	//	node->setPosition(0, 0, 400);
+	//	node->yaw(Ogre::Degree(180.0f));
+	//
+	//	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+	//	Ogre::SceneNode* node2 = node->createChildSceneNode("node2");
+	//	node2->yaw(Ogre::Degree(45));
+	//	node2->translate(0, 0, 20);
+	//	node2->attachObject(ent2);
+	//
+	//	Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+	//	Ogre::SceneNode* node3 = node->createChildSceneNode("node3");
+	//	node3->yaw(Ogre::Degree(45));
+	//	node3->translate(0, 0, 20, Ogre::Node::TS_LOCAL);
+	//	node3->attachObject(ent3);
+	// Part 6>
 
-	mEntity = mSceneMgr->createEntity("crate", "crate.mesh");
-	mEntity->setQueryFlags(1 << 2);
-	mNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("crate");
-	mNode->attachObject(mEntity);
-	mEntity->setCastShadows(true);
+	//<Part 7 - Rotating in different spaces
+	Ogre::Entity* ent2 = mSceneMgr->createEntity("MyEntity2", "sinbad.mesh");
+	Ogre::SceneNode* node2 =
+			mSceneMgr->getRootSceneNode()->createChildSceneNode("Node2");
+	node2->setPosition(10, 0, 0);
+	node2->yaw(Ogre::Degree(90));
+	node2->roll(Ogre::Degree(90));
+	node2->attachObject(ent2);
 
-	mBody = new OgreOde::Body(mWorld);
-	mNode->attachObject(mBody);
+	Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+	Ogre::SceneNode* node3 = node->createChildSceneNode("node3");
+	node3->setPosition(20, 0, 0);
+	node3->yaw(Ogre::Degree(90), Ogre::Node::TS_WORLD);
+	node3->roll(Ogre::Degree(90), Ogre::Node::TS_WORLD);
+	node3->attachObject(ent3);
 
-	Vector3 size(10.0, 10.0, 10.0);
-	OgreOde::BoxMass mMass(0.5, size);
-
-	mMass.setDensity(5.0, size);
-	mGeom = (OgreOde::Geometry*) new OgreOde::BoxGeometry(size, mWorld,
-			mWorld->getDefaultSpace());
-	mNode->setScale(size.x/5.0, size.y/5.0, size.z/5.0);
-	mBody->setMass(mMass);
-	mGeom->setBody(mBody);
-	mEntity->setUserAny(Any(mGeom));
-
-	mBody->setOrientation(Quaternion(Radian(5.0), Vector3(0, 0, 0)));
-	mBody->setPosition(Vector3(0, 120, -20));
-
-}
-
-void Example::createFrameListener(void)
-{
-	mFrameListener = new ExampleFL(mWindow, mCamera, mWorld,
-			mStepper);
-	mRoot->addFrameListener(mFrameListener);
+	// Part 7>
 }
 
--- Example.h
+++ Example.h
@@ -8,33 +8,7 @@
 #ifndef EXAMPLE_H_
 #define EXAMPLE_H_
 
-#include "OgreOde_Core.h"
-
 #include "ExampleApplication.h"
-
-class ExampleFL : public ExampleFrameListener
-{
-protected:
-  OgreOde::World* mWorld;
-  OgreOde::StepHandler* mStepper;
-
-public:
-    ExampleFL(RenderWindow* win, Camera* cam, OgreOde::World* _world,
-OgreOde::StepHandler* _stepper)
-        : ExampleFrameListener(win, cam), mWorld(_world), mStepper(_stepper)
-    {
-    }
-   //bool frameStarted(const FrameEvent& evt)
-   bool frameEnded(const FrameEvent& evt)
-   {
-      Real time = evt.timeSinceLastFrame;
-      if ( mStepper->step(time) )
-            mWorld->synchronise();
-      return ExampleFrameListener::frameEnded(evt);
-      //return ExampleFrameListener::frameStarted(evt);
-   }
-};
-
 
 class Example: public ExampleApplication
 {
@@ -43,19 +17,9 @@ public:
 	virtual ~Example();
 
 	virtual void createScene();
-	virtual void createFrameListener(void);
 
 protected:
 
-	OgreOde::World *mWorld;
-	OgreOde::Space *mSpace;
-	OgreOde::StepHandler *mStepper;
-
-	OgreOde::InfinitePlaneGeometry *mGround;
-	OgreOde::Body *mBody;
-	OgreOde::Geometry *mGeom;
-	Ogre::SceneNode *mNode;
-	Ogre::Entity *mEntity;
 };
 
 #endif /* EXAMPLE_H_ */
