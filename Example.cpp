ced1b451a1c9a7b1244d70352c99503c6aec2032
consultit <consultit@katamail.com>
2011-04-21 10:48:57 +0000

    * "OGRE 3D 1.7 Beginners Guide" Examples from chapter 6 completed.

--- Example.cpp
+++ Example.cpp
@@ -7,233 +7,134 @@
 
 #include "Example.h"
 
-Example::Example()
-{
-	FrameListener = NULL;
-}
-
-Example::~Example()
-{
-	if (FrameListener)
-	{
-		delete FrameListener;
-	}
-}
-
 void Example::createScene()
 {
+	// <Part 1
+	//	std::cout << mSceneMgr->getTypeName() << "::" << mSceneMgr->getName()
+	//			<< std::endl;
+	// Part 1>
+
 	Ogre::Plane plane(Vector3::UNIT_Y, -10);
 	Ogre::MeshManager::getSingleton().createPlane("plane",
 			ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME, plane, 1500,
 			1500, 200, 200, true, 1, 5, 5, Vector3::UNIT_Z);
-	Ogre::Entity* ent = mSceneMgr->createEntity("LightPlaneEntity", "plane");
-	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
-	ent->setMaterialName("Examples/BeachStones");
 
-	mSceneMgr->setShadowTechnique(SHADOWTYPE_STENCIL_ADDITIVE);
-	//	mSceneMgr->setShadowTechnique(SHADOWTYPE_STENCIL_MODULATIVE);
+	Ogre::Entity* ent = mSceneMgr->createEntity("GrassPlane", "plane");
+	mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
+	ent->setMaterialName("Examples/GrassFloor");
 
 	Ogre::Light* light = mSceneMgr->createLight("Light1");
 	light->setType(Ogre::Light::LT_DIRECTIONAL);
 	light->setDirection(Ogre::Vector3(1, -1, 0));
 
-	Ogre::SceneNode* node = mSceneMgr->createSceneNode("Node1");
-	mSceneMgr->getRootSceneNode()->addChild(node);
-	_SinbadEnt = mSceneMgr->createEntity("Sinbad", "Sinbad.mesh");
-	_SinbadNode = node->createChildSceneNode("SinbadNode");
-	_SinbadNode->setScale(3.0f, 3.0f, 3.0f);
-	_SinbadNode->setPosition(Ogre::Vector3(0.0f, 4.8f, 0.0f));
-	_SinbadNode->attachObject(_SinbadEnt);
+	Ogre::ManualObject* manual = mSceneMgr->createManualObject("grass");
 
-	Ogre::Entity* sword1 = mSceneMgr->createEntity("Sword1", "Sword.mesh");
-	Ogre::Entity* sword2 = mSceneMgr->createEntity("Sword2", "Sword.mesh");
-	_SinbadEnt->attachObjectToBone("Handle.L", sword1);
-	_SinbadEnt->attachObjectToBone("Handle.R", sword2);
+	manual->begin("Examples/GrassBlades", RenderOperation::OT_TRIANGLE_LIST);
+	//	manual->begin("Logo", RenderOperation::OT_TRIANGLE_LIST);
+	//1 Quad
+	manual->position(5.0, 0.0, 0.0);
+	manual->textureCoord(1, 1);
+	manual->position(-5.0, 10.0, 0.0);
+	manual->textureCoord(0, 0);
+	manual->position(-5.0, 0.0, 0.0);
+	manual->textureCoord(0, 1);
+	//
+	//	manual->position(5.0, 0.0, 0.0);
+	//	manual->textureCoord(1, 1);
+	manual->position(5.0, 10.0, 0.0);
+	manual->textureCoord(1, 0);
+	//	manual->position(-5.0, 10.0, 0.0);
+	//	manual->textureCoord(0, 0);
+	//2 Quad
+	manual->position(2.5, 0.0, 4.3);
+	manual->textureCoord(1, 1);
+	manual->position(-2.5, 10.0, -4.3);
+	manual->textureCoord(0, 0);
+	manual->position(-2.0, 0.0, -4.3);
+	manual->textureCoord(0, 1);
+	//
+	//	manual->position(2.5, 0.0, 4.3);
+	//	manual->textureCoord(1, 1);
+	manual->position(2.5, 10.0, 4.3);
+	manual->textureCoord(1, 0);
+	//	manual->position(-2.5, 10.0, -4.3);
+	//	manual->textureCoord(0, 0);
+	//3 Quad
+	manual->position(2.5, 0.0, -4.3);
+	manual->textureCoord(1, 1);
+	manual->position(-2.5, 10.0, 4.3);
+	manual->textureCoord(0, 0);
+	manual->position(-2.0, 0.0, 4.3);
+	manual->textureCoord(0, 1);
+	//
+	//	manual->position(2.5, 0.0, -4.3);
+	//	manual->textureCoord(1, 1);
+	manual->position(2.5, 10.0, -4.3);
+	manual->textureCoord(1, 0);
+	//	manual->position(-2.5, 10.0, 4.3);
+	//	manual->textureCoord(0, 0);
 
-	Ogre::AnimationStateSet* set = _SinbadEnt->getAllAnimationStates();
-	Ogre::AnimationStateIterator iter = set->getAnimationStateIterator();
-	while (iter.hasMoreElements())
+	//1 Quad
+	manual->index(0);
+	manual->index(1);
+	manual->index(2);
+	manual->index(0);
+	manual->index(3);
+	manual->index(1);
+	//2 Quad
+	manual->index(4);
+	manual->index(5);
+	manual->index(6);
+	manual->index(4);
+	manual->index(7);
+	manual->index(5);
+	//3 Quad
+	manual->index(8);
+	manual->index(9);
+	manual->index(10);
+	manual->index(8);
+	manual->index(11);
+	manual->index(9);
+
+	manual->end();
+	manual->convertToMesh("BladesOfGrass");
+
+	//	Ogre::SceneNode* grassNode =
+	//			mSceneMgr->getRootSceneNode()->createChildSceneNode("GrassNode2");
+	//	grassNode->attachObject(manual);
+
+	Ogre::StaticGeometry* field = mSceneMgr->createStaticGeometry(
+			"FieldOfGrass");
+
+	for (int i = 0; i < 50; i++)
 	{
-		std::cout << iter.getNext()->getAnimationName() << std::endl;
-	}
-
-}
-
-void Example::createCamera()
-{
-	mCamera = mSceneMgr->createCamera("MyCamera1");
-	mCamera->setPosition(0, 100, 200);
-	mCamera->lookAt(0, 0, 0);
-	mCamera->setNearClipDistance(5);
-}
-
-void Example::createFrameListener(void)
-{
-	FrameListener = new Example4FrameListener(_SinbadNode, _SinbadEnt, mWindow,
-			mCamera);
-	mRoot->addFrameListener(FrameListener);
-}
-
-/*----------------------------------------------------*/
-
-Example4FrameListener::Example4FrameListener(Ogre::SceneNode *node,
-		Ogre::Entity* ent, RenderWindow* win, Ogre::Camera* cam) :
-	_PolyMode(Ogre::PM_SOLID), _speed(40.0)
-{
-	_node = node;
-
-	_ent = ent;
-	//	_aniState = _ent->getAnimationState("Dance");
-	_aniState = _ent->getAnimationState("RunBase");
-	//	_aniState->setEnabled(true);
-	_aniState->setLoop(false);
-	_aniStateTop = _ent->getAnimationState("RunTop");
-	//	_aniStateTop->setEnabled(true);
-	_aniStateTop->setLoop(false);
-	_WalkingSpeed = 50.0f;
-	_rotation = 0.0f;
-
-	size_t windowHnd = 0;
-	std::stringstream windowHndStr;
-	win->getCustomAttribute("WINDOW", &windowHnd);
-	windowHndStr << windowHnd;
-	OIS::ParamList pl;
-	pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
-	_man = OIS::InputManager::createInputSystem(pl);
-	_key = static_cast<OIS::Keyboard*> (_man->createInputObject(
-			OIS::OISKeyboard, false));
-	_mouse = static_cast<OIS::Mouse*> (_man->createInputObject(OIS::OISMouse,
-			false));
-
-	_Cam = cam;
-
-	_timer.reset();
-}
-
-Example4FrameListener::~Example4FrameListener()
-{
-	_man->destroyInputObject(_mouse);
-	_man->destroyInputObject(_key);
-	OIS::InputManager::destroyInputSystem(_man);
-}
-
-bool Example4FrameListener::frameStarted(const FrameEvent & evt)
-{
-	//<Part 1
-	//	_node->translate(Ogre::Vector3(0.1, 0, 0));
-	//Part 1>
-	//<Part 2 - Modifying the code to be time based rather than frame based
-	//	_node->translate(Ogre::Vector3(10, 0, 0) * evt.timeSinceLastFrame);
-	//Part 2>
-
-	Ogre::Vector3 direction(0, 0, 0);
-
-	_key->capture();
-	_mouse->capture();
-
-	if (_key->isKeyDown(OIS::KC_ESCAPE))
-	{
-		return false;
-	}
-	if (_key->isKeyDown(OIS::KC_W))
-	{
-		direction = Ogre::Vector3(0, 0, -1);
-	}
-	if (_key->isKeyDown(OIS::KC_S))
-	{
-		direction = Ogre::Vector3(0, 0, 1);
-	}
-	if (_key->isKeyDown(OIS::KC_A))
-	{
-		direction = Ogre::Vector3(-1, 0, 0);
-	}
-	if (_key->isKeyDown(OIS::KC_D))
-	{
-		direction = Ogre::Vector3(1, 0, 0);
-	}
-	if (_key->isKeyDown(OIS::KC_R) && _timer.getMilliseconds() > 250)
-	{
-		if (_PolyMode == PM_SOLID)
+		for (int j = 0; j < 50; j++)
 		{
-			_PolyMode = Ogre::PM_WIREFRAME;
-		}
-		else if (_PolyMode == PM_WIREFRAME)
-		{
-			_PolyMode = Ogre::PM_POINTS;
-		}
-		else if (_PolyMode == PM_POINTS)
-		{
-			_PolyMode = Ogre::PM_SOLID;
-		}
-		_Cam->setPolygonMode(_PolyMode);
-		_timer.reset();
-	}
+			Ogre::Entity * ent = mSceneMgr->createEntity("BladesOfGrass");
+			//			Ogre::SceneNode* node =
+			//					mSceneMgr->getRootSceneNode()->createChildSceneNode(
+			//							Ogre::Vector3(i * 3, -10, j * 3));
+			//			node->attachObject(ent);
+			field->addEntity(ent, Ogre::Vector3(i * 3, -10, j * 3));
 
-	// <Part 3 - Adding movement to the model
-	//	_node->translate(speed * direction * evt.timeSinceLastFrame);
-	// Part 3>
-	float rotX = _mouse->getMouseState().X.rel * evt.timeSinceLastFrame * -1;
-	float rotY = _mouse->getMouseState().Y.rel * evt.timeSinceLastFrame * -1;
-	_Cam->yaw(Ogre::Radian(rotX));
-	_Cam->pitch(Ogre::Radian(rotY));
-	_Cam->moveRelative(direction * evt.timeSinceLastFrame * _speed);
-
-	bool walked = false;
-	Ogre::Vector3 SinbadTranslate(0, 0, 0);
-	if (_key->isKeyDown(OIS::KC_UP))
-	{
-		SinbadTranslate += Ogre::Vector3(0, 0, -1);
-		_rotation = 3.14f;
-		walked = true;
-	}
-	if (_key->isKeyDown(OIS::KC_DOWN))
-	{
-		SinbadTranslate += Ogre::Vector3(0, 0, 1);
-		_rotation = 0.0f;
-		walked = true;
-	}
-	if (_key->isKeyDown(OIS::KC_LEFT))
-	{
-		SinbadTranslate += Ogre::Vector3(-1, 0, 0);
-		_rotation = -1.57f;
-		walked = true;
-	}
-	if (_key->isKeyDown(OIS::KC_RIGHT))
-	{
-		SinbadTranslate += Ogre::Vector3(1, 0, 0);
-		_rotation = 1.57f;
-		walked = true;
-	}
-
-	if (walked)
-	{
-		_aniState->setEnabled(true);
-		_aniStateTop->setEnabled(true);
-		if (_aniState->hasEnded())
-		{
-			_aniState->setTimePosition(0.0f);
+			//			std::cout << node->getName() << "::" << ent->getName() << std::endl;
 		}
-		if (_aniStateTop->hasEnded())
-		{
-			_aniStateTop->setTimePosition(0.0f);
-		}
-		_aniState->addTime(evt.timeSinceLastFrame);
-		_aniStateTop->addTime(evt.timeSinceLastFrame);
 	}
-	else
-	{
-		_aniState->setTimePosition(0.0f);
-		_aniState->setEnabled(false);
-		_aniStateTop->setTimePosition(0.0f);
-		_aniStateTop->setEnabled(false);
-	}
-	_node->translate(SinbadTranslate * evt.timeSinceLastFrame * _WalkingSpeed);
-	_node->resetOrientation();
-	_node->yaw(Ogre::Radian(_rotation));
-
-	//	_aniState->addTime(evt.timeSinceLastFrame);
-	//	_aniStateTop->addTime(evt.timeSinceLastFrame);
-
-	return true;
+	field->build();
 }
 
+// <Part 1
+//void Example::chooseSceneManager(void)
+//{
+//	ResourceGroupManager::getSingleton().addResourceLocation(
+//			"/REPOSITORY/KProjects/usr/share/OGRE/media/packs/chiropteraDM.pk3",
+//			"Zip",
+//			ResourceGroupManager::getSingleton().getWorldResourceGroupName(),
+//			true);
+//	ResourceGroupManager::getSingleton().initialiseResourceGroup(
+//			ResourceGroupManager::getSingleton().getWorldResourceGroupName());
+//	mSceneMgr = mRoot->createSceneManager("BspSceneManager");
+//	mSceneMgr->setWorldGeometry("maps/chiropteradm.bsp");
+//
+//}
+// Part 1>
--- Example.h
+++ Example.h
@@ -13,46 +13,15 @@
 class Example: public ExampleApplication
 {
 public:
-	Example();
-	virtual ~Example();
 
 protected:
+
 	virtual void createScene();
-	virtual void createCamera();
-	virtual void createFrameListener(void);
+	//<Part 1
+	//	virtual void chooseSceneManager(void);
+	// Part 1>
 
 private:
-	Ogre::SceneNode* _SinbadNode;
-	Ogre::Entity* _SinbadEnt;
-	Ogre::FrameListener* FrameListener;
-};
-
-class Example4FrameListener: public Ogre::FrameListener
-{
-public:
-	Example4FrameListener(Ogre::SceneNode* node, Ogre::Entity* ent,
-			RenderWindow* win, Ogre::Camera* cam);
-	virtual ~Example4FrameListener();
-
-	virtual bool frameStarted(const FrameEvent& evt);
-
-private:
-	Ogre::SceneNode* _node;
-	Ogre::Camera* _Cam;
-	Ogre::PolygonMode _PolyMode;
-	Ogre::Timer _timer;
-
-	Ogre::Entity* _ent;
-	Ogre::AnimationState* _aniState;
-	Ogre::AnimationState* _aniStateTop;
-	float _WalkingSpeed;
-	float _rotation;
-
-	OIS::InputManager* _man;
-	OIS::Keyboard* _key;
-	OIS::Mouse* _mouse;
-
-	float _speed;
 
 };
 
