e17733f296c48e3f5fd88ac3cd0f0059fdda657e
consultit <consultit@katamail.com>
2011-07-25 16:25:31 +0000

    * Converted (manually) to an autoconf project.

---
+++ game/Ely.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Ely.cpp
+ Author      : Marco Paone
+ Version     :
+ Copyright   : GPL licensed
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Ely" << endl; /* prints Ely */
+	return 0;
+}
