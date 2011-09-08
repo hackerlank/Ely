65b7ecbb196a99ad1361c2483e8d3a10864f3e91
consultit <consultit@katamail.com>
2011-09-08 11:29:30 +0000

    * Started to add GOCS, from an adaptation of  "Game Object Component System" page 393 - Chris Stoy - "Game Programming Gems 6" (2006). * added class GameObject.

---
+++ src/Game/GameObject.cpp
@@ -0,0 +1,20 @@
+/**
+ * \file GameObject.cpp
+ *
+ * \date 08/set/2011
+ * \author Marco Paone
+ */
+
+#include "Game/GameObject.h"
+
+GameObject::GameObject()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+GameObject::~GameObject()
+{
+	// TODO Auto-generated destructor stub
+}
+
