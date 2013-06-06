fc1d4b07ff9e878940e8a4bc3cee3bba5590e893
consultit <consultit@katamail.com>
2013-06-06 18:11:19 +0000

    * AIComponents: started to add CrowdAgent and related template.

---
+++ include/AIComponents/CrowdAgent.h
@@ -0,0 +1,34 @@
+/*
+ *   This file is part of Ely.
+ *
+ *   Ely is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Ely is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Ely.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * \file /Ely/include/AIComponents/CrowdAgent.h
+ *
+ * \date 06/giu/2013 (19:27:21)
+ * \author consultit
+ */
+
+#ifndef CROWDAGENT_H_
+#define CROWDAGENT_H_
+
+class CrowdAgent
+{
+public:
+	CrowdAgent();
+	virtual ~CrowdAgent();
+};
+
+#endif /* CROWDAGENT_H_ */
---
+++ include/AIComponents/CrowdAgentTemplate.h
@@ -0,0 +1,34 @@
+/*
+ *   This file is part of Ely.
+ *
+ *   Ely is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Ely is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Ely.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * \file /Ely/include/AIComponents/CrowdAgentTemplate.h
+ *
+ * \date 06/giu/2013 (19:29:33)
+ * \author consultit
+ */
+
+#ifndef CROWDAGENTTEMPLATE_H_
+#define CROWDAGENTTEMPLATE_H_
+
+class CrowdAgentTemplate
+{
+public:
+	CrowdAgentTemplate();
+	virtual ~CrowdAgentTemplate();
+};
+
+#endif /* CROWDAGENTTEMPLATE_H_ */
