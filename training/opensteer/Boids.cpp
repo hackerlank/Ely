9feb0eded83dcfd76abaa37df474ea4197b0890d
consultit <consultit@katamail.com>
2013-11-27 20:08:12 +0000

    * training: opensteer: started to add the Boids plugin. There are compilation errors.

---
+++ training/opensteer/Boids.cpp
@@ -0,0 +1,50 @@
+// ----------------------------------------------------------------------------
+//
+//
+// OpenSteer -- Steering Behaviors for Autonomous Characters
+//
+// Copyright (c) 2002-2005, Sony Computer Entertainment America
+// Original author: Craig Reynolds <craig_reynolds@playstation.sony.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//
+// ----------------------------------------------------------------------------
+//
+//
+// OpenSteer Boids
+// 
+// 09-26-02 cwr: created 
+//
+//
+// ----------------------------------------------------------------------------
+
+#include "Boids.h"
+
+namespace ely
+{
+
+AVGroup _Boid::neighbors;
+float _Boid::worldRadius = 50.0f;
+ObstacleGroup _Boid::obstacles;
+#ifndef NO_LQ_BIN_STATS
+size_t _Boid::minNeighbors, _Boid::maxNeighbors, _Boid::totalNeighbors;
+#endif // NO_LQ_BIN_STATS
+
+}// anonymous namespace
