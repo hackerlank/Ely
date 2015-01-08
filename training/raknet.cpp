133414dbeceead4d313d6eb78ca659de3a2a880b
consultit <consultit@katamail.com>
2015-01-08 13:22:20 +0100

    Training: continuing with raknet...

--- training/raknet.cpp
+++ training/raknet.cpp
@@ -27,6 +27,9 @@
 #include <geomLines.h>
 #include <lpoint3.h>
 #include <iostream>
+#include <raknet/MessageIdentifiers.h>
+#include <raknet/RakPeerInterface.h>
+#include <raknet/RakNetTypes.h>
 
 using namespace std;
 
@@ -66,10 +69,78 @@ int raknet_main(int argc, char *argv[])
 	trackball->set_hpr(0, 15, 0);
 
 	///here is room for your own code
+	enum
+	{
+		CLIENT, SERVER
+	} peerType = CLIENT;
+	string serverIP = "127.0.0.1";
+	unsigned short int serverPort = 20000;
+	unsigned int maxConnectionsAllowed = 2;
+	unsigned short int maxPlayersPerServer = 2;
+	// get parameters
+	int c;
+	opterr = 0;
+	while ((c = getopt(argc, argv, "csa:p:m:n:")) != -1)
+	{
+		switch (c)
+		{
+		case 'c':
+			peerType = CLIENT;
+			break;
+		case 's':
+			peerType = SERVER;
+			break;
+		case 'a':
+			serverIP = string(optarg);
+			break;
+		case 'p':
+			serverPort = (unsigned short int) atoi(optarg);
+			break;
+		case 'm':
+			maxConnectionsAllowed = (unsigned int) atoi(optarg);
+			break;
+		case 'n':
+			maxPlayersPerServer = (unsigned short int) atoi(optarg);
+			break;
+		case '?':
+			if ((optopt == 'a') or (optopt == 'p') or (optopt == 'm')
+					or (optopt == 'n'))
+				std::cerr << "Option " << optopt << " requires an argument.\n"
+						<< std::endl;
+			else if (isprint(optopt))
+				std::cerr << "Unknown option " << optopt << std::endl;
+			else
+				std::cerr << "Unknown option character " << optopt << std::endl;
+			return 1;
+		default:
+			abort();
+		}
+	}
+	//
+	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+	//connect
+	switch (peerType)
+	{
+	case CLIENT:
+	{
+		RakNet::SocketDescriptor clientSocket = RakNet::SocketDescriptor();
+		peer->Startup(1, &clientSocket, 1);
+		peer->Connect(serverIP.c_str(), serverPort, 0, 0);
+	}
+		break;
+	case SERVER:
+	{
+		RakNet::SocketDescriptor serverSocket(serverPort, 0);
+		peer->Startup(maxConnectionsAllowed, &serverSocket, 1);
+		peer->SetMaximumIncomingConnections(maxPlayersPerServer);
+	}
+		break;
+	default:
+		abort();
+	}
 
 	//do the main loop, equal to run() in python
 	framework.main_loop();
-
 	//close the window framework
 	framework.close_framework();
 	return (0);
