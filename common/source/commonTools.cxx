80f6db8118574d3a943a783aebf909504b238afc
consultit <consultit@katamail.com>
2016-10-09 10:04:36 +0200

    Removed template explicit instantiations (2).

--- common/source/commonTools.cxx
+++ common/source/commonTools.cxx
@@ -120,9 +120,3 @@ void ThrowEventData::read_datagram(DatagramIterator &scan)
 	mFrequency = scan.get_stdfloat();
 	mPeriod = scan.get_stdfloat();
 }
-
-///ValueList template
-// Tell GCC that we'll take care of the instantiation explicitly here.
-#ifdef __GNUC__
-#pragma implementation
-#endif
